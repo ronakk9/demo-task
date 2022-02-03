7b4cb5fea31bcd792a021d239fd192023c9e9d85
ronakk9 <ronakkhandelwal@gmail.com>
2022-02-03 14:56:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import TaskForm from './Components/TaskForm';
+import { useState } from 'react';
 
 function App() {
+
+  const api = {
+    key: "9777cb2c188bfdc40ae4624c4c085b23",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  const [weather,setWeather] = useState("");
+  const [city,setCity] = useState("");
+
+  function handleChange(event){
+    setCity(event.target.value);
+  }
+  function handleTemp(){
+    fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`).then((data) => data = data.json())
+    .then((info) => setWeather(info))
+    console.log(weather);
+    document.getElementById("smthng").innerHTML = weather.main.temp;
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="weather-app">
+      <input placeholder='Enter city name...' value={city} onChange={handleChange}></input>
+      <button onClick={handleTemp}>SUBMIT</button>
+
+      <h2 id="smthng"></h2>
+    </div>
     </div>
   );
 }
